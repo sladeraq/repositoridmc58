0a6103f3fc9e8dd371b830c574a670c106e9a156
sladeraq <sladera@gmail.com>
2026-05-12 19:50:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,4 @@ import streamlit as st
 st.title("Mi primera aplicación en python")
 
 st.sidebar.title("Parámetros")
+sesion = st.sidebar.selectbox("Seleccione una sesión", ["Sesión 1","Sesión 2","Sesión 3","Sesión 4"] )
