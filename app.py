8a20dcb32717301521f641618284efcc1c850d75
sladeraq <sladera@gmail.com>
2026-05-14 21:15:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import numpy as np
 
 st.title("Mi primera aplicación en python")
 
@@ -25,9 +26,16 @@ elif sesion == "Sesión 2":
   st.write("El precio final del producto es: ", precio_final_producto  )
 
 
-
 elif sesion == "Sesión 3":
   st.write("Bienvenido la sesión 3")
-  inicio_rango = st.slider("Selecione un valor",min_value = 0 , max_value=20, value =7 )
+  
+  fin_rango = st.slider("Selecione un valor",min_value = 0 , max_value=20, value =7 )
+
+  arreglo = np.arange(0 , fin_rango)
+
+  st.write(arreglo)
+
+
+
 else:
   st.write("Bienvenido la sesión 4")
